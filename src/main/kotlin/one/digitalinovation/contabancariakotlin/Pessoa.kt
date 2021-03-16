83c30ef98238cb6fc28abf4a713d9e8c0f2b2096
BrenoQGC <brenoqcarvalho@gmail.com>
2021-03-16 15:52:24 -0300

    Conceito de inner class e private set

--- src/main/kotlin/one/digitalinovation/contabancariakotlin/Pessoa.kt
+++ src/main/kotlin/one/digitalinovation/contabancariakotlin/Pessoa.kt
@@ -3,6 +3,7 @@ package one.digitalinovation.contabancariakotlin
 class Pessoa {
     var nome: String = "Breno"
     var cpf: String = "123.1263.123-11"
+    private set // impede alterarações fora do escopo da classe
 
     inner class Endereco{
         var rua: String = "Arquimedes"
@@ -10,6 +11,7 @@ class Pessoa {
 }
 fun main(){
     val breno = Pessoa()
+    // breno.cpf="123"// impedido pelo private set dentro
 
     println(breno.nome)
     println(breno.cpf)
